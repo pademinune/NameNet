921d225560c208db33217b5de7742dfbbb29e57d
pademinune <justinnitoi@gmail.com>
2025-07-23 17:21:22 +0300

    added timing

--- training.py
+++ training.py
@@ -8,6 +8,8 @@ import architectures.v1 as v1
 import architectures.v2 as v2
 import architectures.r1 as r1
 
+import time
+
 m: r1.Model = r1.Model()
 # for features, labels in dataloader:
 #     print(labels)
@@ -22,10 +24,16 @@ loss_fn = nn.CrossEntropyLoss()
 #     print(features.shape)
 #     print(features[0])
 
-epochs: int = 200
+epochs: int = 10
 loss_interval: int = max(1, epochs // 10)
 
+print(f"Training started on {epochs} epochs.")
+
 for epoch in range(epochs):
+    
+    if epoch % loss_interval == 0:
+        start = time.time()
+
     epoch_loss: float = 0
     num_batches: int = 0
 
@@ -43,7 +51,9 @@ for epoch in range(epochs):
         optimizer.step()
 
     if epoch % loss_interval == 0:
-        print(f"Epoch {epoch} loss: {epoch_loss/num_batches:.5f}")
+        end = time.time()
+        duration = end - start
+        print(f"Epoch {epoch} loss: {epoch_loss/num_batches:.5f}\t\t\t{duration:.3f}s")
 
 torch.save(m.state_dict(), "trained.model")
 print("TRAINING FINISHED")
